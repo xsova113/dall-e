fb3cc73a962e1b4ed5a3298c99b06a5c9e321dea
George <xsova113@gmail.com>
2023-04-18 17:25:26 -0700

    change fetching url for backend api url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/v1/post', {
+      const response = await fetch('https://dall-e-h6pz.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
